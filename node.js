a0da57b0bae00ecf9dab25d9e7706263c4c51e6d
David <mr.hilleke@gmail.com>
2022-10-10 23:12:29 -0400

    work in progress with brandon

--- node.js
+++ node.js
@@ -1,41 +1,126 @@
 const express = require('express');
 const app = express();
-
-//TEMPLATE ENGINE
-app.set('view engine', 'ejs');
-
-const db = require('./models')
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-
-//ROUTERS
-const adminRouter = require('./routes/admin')
-app.use('/admin', adminRouter)
-const interactionRouter = require('./routes/interaction')
-app.use('/interaction', interactionRouter)
+const db = require('./models');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const store = new SequelizeStore({ db: db.sequelize});
+const bcrypt = require('bcrypt');
+const adminRouter = require('./routes/admin');
+const interactionRouter = require('./routes/interaction');
 const customerRouter = require('./routes/customer');
 const { RowDescriptionMessage } = require('pg-protocol/dist/messages');
 const { sequelize, Sequelize } = require('./models');
 const { DATE } = require('sequelize');
-app.use('/customers', customerRouter)
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res)=>{
-    console.log({type: Sequelize.DATE})
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use('/admin', checkAuth, adminRouter);
+app.use('/interaction', checkAuth, interactionRouter);
+app.use('/customers', checkAuth, customerRouter);
+app.use(
+    session({
+        secret:'secret',
+        resave: false,
+        saveUninitialized: true,
+        store: store,
+    }),
+);
+store.sync();
+
+
+//ROUTES
+app.get('/', (req, res) => {
+    // console.log({type: Sequelize.DATE})
     res.render('home');
 });
 
-app.post('/customer/:slug', async (req, res) =>{
+
+function checkAuth(req, res, next) {
+    if (req.session.user){
+        next();
+    } else {
+        alert("Please log in before proceeding."),
+        res.redirect('/')
+    }
+};
+
+//Do we need to adjust the sequelize model to accept new admins from register page?
+//then store session data in sequelize session DB instead of local/cookies 
+
+//REGISTER ROUTE
+app.get('/register', (req, res) => {
+    res.render('./register')
+})
+
+//REGISTER ROUTE
+app.post('/register', (req, res) => {
+    const {name, email, phone, password} = req.body;
+    bcrypt.hash(req.body.password, 10, async (err, hash) => {
+        let newAdmin = await db.Admin.create({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: hash,
+        })
+        .then((result) => {
+            console.log('it works')
+            res.redirect('/')
+        })
+    })
+});
+
+// app.post('/register', async (req, res) => {
+//     const hash = bcrypt.hashSync(req.body.password, 10);
+//     let newAdmin = await db.Admin.create({
+//         name: req.session.name,
+//         email: req.session.email,
+//         phone: req.session.phone,
+//         password: hash,
+//     })
+//     .then((result) => {
+//         res.redirect('/');
+//     })
+// });
+
+
+//LOGIN ROUTE
+app.post('/', (req, res) => {
+    const {username, password} = req.body;
+    db.Admin.findOne({
+        where: {username},
+    }).then((Admin) => {
+        bcrypt.compare(password, Admin.password, (err, match) => {
+            if(match) {
+                //handle successful login
+                res.send('password matched')
+            } else {
+                //handle login failure
+                res.send('password did not match')
+            }
+        })
+    })
+})
+
+
+app.post('/customer/:slug', checkAuth, async (req, res) =>{
     let newInteraction = await db.Interaction.create({
         customerId: req.body.custId,
         adminId: req.body.selectID,
         type: req.body.selectType,
         note: req.body.inputNote,
     })
-    res.redirect('back');
+    res.redirect('back', {
+        user: req.session.user,
+    });
 });
 
-app.post('/customer', async (req, res) =>{
+
+app.post('/customer', checkAuth, async (req, res) =>{
     let newCustomer = await db.Customer.create({
         firstName: req.body.inputFirstName,
         lastName: req.body.inputLastName,
@@ -44,11 +129,13 @@ app.post('/customer', async (req, res) =>{
         city: req.body.inputCity,
         state: req.body.selectState,
     })
-    res.redirect('back');
+    res.redirect('back', {
+        user: req.session.user,
+    });
 });
 
 
-var server = app.listen(4000, function() {
+const server = app.listen(4000, function() {
     console.log('Server running at http://localhost:4000/')
 });
 
--- routes/interaction.js
+++ routes/interaction.js
@@ -1,37 +1,33 @@
 const express = require('express');
 const Sequelize = require('sequelize');
 const { Interaction } = require('../models/interaction');
+const { Admin } = require('../models/admin');
 const router = express.Router();
 const db = require('../models')
 
 
 router.get('/', function(req, res, next) {
-    Promise.all([db.Interaction.findAll(),
-        db.Admin.findAll(),
-        db.Customer.findAll(),
+    Promise.all([db.Admin.findAll(), 
+                 db.Interaction.findAll(),
+                db.Customer.findAll()
     ])
     .then((data) => {
-        const customers = [];
-        const admins =[];
-        data[2].forEach((customer) =>{
-            customers.push({
-                firstName: customer.firstName,
-                lastName: customer.lastName
+        const customerInteractions = [];
+        data[1].forEach((interaction) => {
+            customerInteractions.push({
+                id: interaction.id,
+                type: interaction.type,
+                createdAt: interaction.createdAt,
+                note: interaction.note,
+                firstName: data[2].find((x) => x.id === interaction.customerId)?.firstName,
+                lastName: data[2].find((x) => x.id === interaction.customerId)?.lastName,
+                adminName: data[0].find((x) => x.id === interaction.adminId)?.name,
             })
         })
-        data[1].forEach((admin) =>{
-            admins.push({
-                adminName: admin.name,
-            })
-        })
-       res.render('../views/interaction', {interactionData:data[0], customers, admins})
+        console.log(customerInteractions);
+        res.render('../views/interaction', {adminData:data[0], customerInteractions})
     });
 });
 
+
 module.exports = router;
-
-
-
-
-
-
